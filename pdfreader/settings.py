788720d31dabf93be15fee349b0926659d3b59ac
Ethan John <97091148+johnmanjohnston@users.noreply.github.com>
2022-12-09 14:00:10 +0400

    Update settings.py

--- pdfreader/settings.py
+++ pdfreader/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s=^3#6&6piacx*p%(%95xkgpmw0li1%9%7%58qx0&eb-c=*vgm'
+SECRET_KEY = 'insert your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
